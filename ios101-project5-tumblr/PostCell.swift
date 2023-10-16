bffc6eb5dd51e1eb8ffcb90105c5f3905a65f487
jiwagon <jiwagon11@gmail.com>
2023-10-16 00:48:47 -0400

    Added post cell

---
+++ ios101-project5-tumblr/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Zhang Ji on 10/16/23.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
